d967ebbba97281acaee500fd1e6499dfc13dfd45
MOCSKILL <lomoctar1234@gmail.com>
2024-12-05 00:12:43 -0500

    Mise a jour avec les desnier modifications

--- JS/main.js
+++ JS/main.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
     $(".filter-item").click(function () {
         const value = $(this).attr("data-filter");
         if (value == "all") {
-            $(".post-box").show('1000');
+            $(".cours-box").show('1000');
         } else {
-            $(".post-box")
+            $(".cours-box")
                 .not("." + value)
                 .hide("1000");
-            $(".post-box")
+            $(".cours-box")
                 .filter("." + value)
                 .show("1000");
         }
