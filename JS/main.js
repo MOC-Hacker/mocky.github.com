a26cb4bbdcf0f2fedddeaf39a3742b7b78b0d03f
MOCSKILL <lomoctar1234@gmail.com>
2024-12-07 23:38:14 -0500

    Ajout de vours 6

--- JS/main.js
+++ JS/main.js
@@ -25,3 +25,28 @@ let header = document.querySelector("header");
 window.addEventListener("scroll", () => {
     header.classList.toggle("shadow", window.scrollY > 0);
 });
+
+const themeToggleButton = document.querySelector('.theme-toggle');
+const body = document.body;
+
+// Mémoriser le thème dans localStorage
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    body.classList.remove('light-mode', 'dark-mode');
+    body.classList.add(savedTheme);
+    themeToggleButton.innerHTML = savedTheme === 'dark-mode' ? '<i class="bx bx-moon"></i>' : '<i class="bx bx-sun"></i>';
+}
+
+themeToggleButton.addEventListener('click', () => {
+    if (body.classList.contains('light-mode')) {
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+        themeToggleButton.innerHTML = '<i class="bx bx-moon"></i>';
+        localStorage.setItem('theme', 'dark-mode'); // Sauvegarder le thème dans localStorage
+    } else {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+        themeToggleButton.innerHTML = '<i class="bx bx-sun"></i>';
+        localStorage.setItem('theme', 'light-mode'); // Sauvegarder le thème dans localStorage
+    }
+});
